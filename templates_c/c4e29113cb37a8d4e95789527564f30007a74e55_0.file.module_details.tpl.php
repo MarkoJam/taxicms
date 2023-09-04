b3083d7b5a328bd55fbac4e1c551e8a8589a4fa4
SrdjanJam <97429881+SrdjanJam@users.noreply.github.com>
2023-09-04 08:27:05 +0200

    'xx'

--- templates_c/c4e29113cb37a8d4e95789527564f30007a74e55_0.file.module_details.tpl.php
+++ templates_c/c4e29113cb37a8d4e95789527564f30007a74e55_0.file.module_details.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.32, created on 2023-08-31 10:34:58
+/* Smarty version 3.1.32, created on 2023-09-04 08:25:28
   from 'C:\wamp\www\taxicms\templates\module_details.tpl' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.32',
-  'unifunc' => 'content_64f050b28045e9_86524948',
+  'unifunc' => 'content_64f5785815c720_22226362',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     'c4e29113cb37a8d4e95789527564f30007a74e55' => 
     array (
       0 => 'C:\\wamp\\www\\taxicms\\templates\\module_details.tpl',
-      1 => 1693222861,
+      1 => 1693481422,
       2 => 'file',
     ),
   ),
@@ -20,7 +20,7 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_64f050b28045e9_86524948 (Smarty_Internal_Template $_smarty_tpl) {
+function content_64f5785815c720_22226362 (Smarty_Internal_Template $_smarty_tpl) {
 if ($_smarty_tpl->tpl_vars['plugin_view']->value == "module_details") {?>
 	<section class="news-details-area">
 		<div class="container">
@@ -28,32 +28,24 @@ if ($_smarty_tpl->tpl_vars['plugin_view']->value == "module_details") {?>
 				<div class="path path-news">
 					<ul>
 						<li><a href="<?php echo $_smarty_tpl->tpl_vars['ROOT_WEB']->value;
-echo $_smarty_tpl->tpl_vars['lang']->value;
-echo $_smarty_tpl->tpl_vars['PLG_LINK_NEWS']->value;?>
-"><?php echo $_smarty_tpl->tpl_vars['PLG_NEWS']->value;?>
+echo $_smarty_tpl->tpl_vars['lang']->value;?>
+/<?php echo $_smarty_tpl->tpl_vars['PLG_LINK_MODULE']->value;?>
+"><?php echo $_smarty_tpl->tpl_vars['PLG_MODULES']->value;?>
 </a></li>
 						<li><?php echo $_smarty_tpl->tpl_vars['module_detail']->value['header'];?>
 </li>
 					</ul>
 				</div>
 			</div>
-			<div class="row">
-				<div class="col-md-12">
-					<div class="gp-title">
-						<h2><?php echo $_smarty_tpl->tpl_vars['module_detail']->value['header'];?>
-</h2>
-					</div>
-				</div>
-			</div>
-			<div class="row">
-				<div class="col-lg-12 col-md-12 col-12">
+						<div class="row">
+				<div class="col-lg-6 col-md-6 col-6">
 					<img src="<?php echo $_smarty_tpl->tpl_vars['ROOT_WEB']->value;
 echo $_smarty_tpl->tpl_vars['module_detail']->value['slika'];?>
 " class="mb-10 w-100" />
-					
-					<?php echo $_smarty_tpl->tpl_vars['module_detail']->value['html'];?>
-
-					<h4>Menu options</h4>
+				</div>	
+				<div class="col-lg-6 col-md-6 col-6">
+					<p><?php echo $_smarty_tpl->tpl_vars['PLG_MENU_OPTIONS']->value;?>
+</p>
 					<?php
 $__section_pom_0_loop = (is_array(@$_loop=$_smarty_tpl->tpl_vars['conmodule']->value) ? count($_loop) : max(0, (int) $_loop));
 $__section_pom_0_total = $__section_pom_0_loop;
@@ -62,12 +54,12 @@ if ($__section_pom_0_total !== 0) {
 for ($__section_pom_0_iteration = 1, $_smarty_tpl->tpl_vars['__smarty_section_pom']->value['index'] = 0; $__section_pom_0_iteration <= $__section_pom_0_total; $__section_pom_0_iteration++, $_smarty_tpl->tpl_vars['__smarty_section_pom']->value['index']++){
 ?>
 						<div class="row">
-							<div class="col-lg-3 col-md-3 col-3">
+							<div class="col-lg-4 col-md-4 col-4">
 								<div class="box-text"><a href='<?php echo $_smarty_tpl->tpl_vars['conmodule']->value[(isset($_smarty_tpl->tpl_vars['__smarty_section_pom']->value['index']) ? $_smarty_tpl->tpl_vars['__smarty_section_pom']->value['index'] : null)]['link_print_dt'];?>
 '><?php echo $_smarty_tpl->tpl_vars['conmodule']->value[(isset($_smarty_tpl->tpl_vars['__smarty_section_pom']->value['index']) ? $_smarty_tpl->tpl_vars['__smarty_section_pom']->value['index'] : null)]['header'];?>
 </a></div>
 							</div>
-							<div class="col-lg-3 col-md-3 col-3">
+							<div class="col-lg-8 col-md-8 col-8">
 								<small><i><?php echo $_smarty_tpl->tpl_vars['conmodule']->value[(isset($_smarty_tpl->tpl_vars['__smarty_section_pom']->value['index']) ? $_smarty_tpl->tpl_vars['__smarty_section_pom']->value['index'] : null)]['shorthtml'];?>
 </i></small>
 							</div>
@@ -77,6 +69,8 @@ for ($__section_pom_0_iteration = 1, $_smarty_tpl->tpl_vars['__smarty_section_po
 }
 ?>
 				</div>
+				<?php echo $_smarty_tpl->tpl_vars['module_detail']->value['html'];?>
+				
 			</div>			
 
 			<?php if ($_smarty_tpl->tpl_vars['module_detail']->value['img_rows'] != '') {?>
